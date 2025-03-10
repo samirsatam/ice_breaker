d56a4421ffb5f1f9e55a38edd30316528155ea5e
Samir Satam <ssatam@gmail.com>
2025-03-09 20:45:16 -0400

    Linked in data supplied as a gist file.

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,51 +1,32 @@
-from typing import Tuple
+from langchain_core.prompts import PromptTemplate
+from langchain_ollama import ChatOllama
+from langchain_openai import ChatOpenAI
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
-from agents.twitter_lookup_agent import lookup as twitter_lookup_agent
-from chains.custom_chains import (
-    get_summary_chain,
-    get_interests_chain,
-    get_ice_breaker_chain,
-)
-from third_parties.linkedin import scrape_linkedin_profile
-from third_parties.twitter import scrape_user_tweets, scrape_user_tweets_mock
-from output_parsers import (
-    Summary,
-    IceBreaker,
-    TopicOfInterest,
-)
-
-
-def ice_break_with(
-    name: str,
-) -> Tuple[Summary, TopicOfInterest, IceBreaker, str]:
-    linkedin_username = linkedin_lookup_agent(name=name)
-    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username)
-
-    twitter_username = twitter_lookup_agent(name=name)
-    tweets = scrape_user_tweets_mock(username=twitter_username)
-
-    summary_chain = get_summary_chain()
-    summary_and_facts: Summary = summary_chain.invoke(
-        input={"information": linkedin_data, "twitter_posts": tweets},
-    )
-
-    interests_chain = get_interests_chain()
-    interests: TopicOfInterest = interests_chain.invoke(
-        input={"information": linkedin_data, "twitter_posts": tweets}
-    )
-
-    ice_breaker_chain = get_ice_breaker_chain()
-    ice_breakers: IceBreaker = ice_breaker_chain.invoke(
-        input={"information": linkedin_data, "twitter_posts": tweets}
-    )
-
-    return (
-        summary_and_facts,
-        interests,
-        ice_breakers,
-        linkedin_data.get("photoUrl"),
-    )
-
 
 if __name__ == "__main__":
-    pass
+    print("hello langchain")
+
+    summary_template = """
+        given the information {information} about a person from I want you to create:
+        1. a short summary
+        2. two interesting facts about them
+    """
+
+    information = """
+   George Washington Carver (c. 1864[1] – January 5, 1943) was an American agricultural scientist and inventor who promoted alternative crops to cotton and methods to prevent soil depletion.[2] He was one of the most prominent black scientists of the early 20th century.
+
+While a professor at Tuskegee Institute, Carver developed techniques to improve types of soils depleted by repeated plantings of cotton. He wanted poor farmers to grow other crops, such as peanuts and sweet potatoes, as a source of their own food and to improve their quality of life.[3] Under his leadership, the Experiment Station at Tuskegee published over forty practical bulletins for farmers, many written by him, which included recipes; many of the bulletins contained advice for poor farmers, including combating soil depletion with limited financial means, producing bigger crops, and preserving food.
+
+Apart from his work to improve the lives of farmers, Carver was also a leader in promoting environmentalism.[4] He received numerous honors for his work, including the Spingarn Medal of the NAACP. In an era of high racial polarization, his fame reached beyond the black community. He was widely recognized and praised in the white community for his many achievements and talents. In 1941, Time magazine dubbed Carver a "Black Leonardo".[5] 
+    """
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
+    # llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
+    # llm = ChatOllama(model="llama3.1")
+    llm = ChatOllama(model="mistral")
+    # create a chain
+    chain = summary_prompt_template | llm
+    res = chain.invoke(input={"information": information})
+    print(res)
--- third_parties/linkedin.py
+++ third_parties/linkedin.py
@@ -4,44 +4,24 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = False):
-    """scrape information from LinkedIn profiles,
-    Manually scrape the information from the LinkedIn profile"""
+    """ scrape information from linkedin profiles,
+    Manually scrape the information from the linkedin profile"""
 
     if mock:
-        linkedin_profile_url = "https://gist.githubusercontent.com/emarco177/859ec7d786b45d8e3e3f688c6c9139d8/raw/32f3c85b9513994c572613f2c8b376b633bfc43f/eden-marco-scrapin.json"
-        response = requests.get(
-            linkedin_profile_url,
-            timeout=10,
-        )
+        linkedin_profile_url= "https://gist.githubusercontent.com/samirsatam/e0b9d14cae4e8b32edd3f2504f08d5b8/raw/b9e78b571af516e8c7685da1539dca6e63543383/samir-satam-scrapin.json"
+        response = requests.get(linkedin_profile_url, timeout=10)
     else:
-        # ENROLL WITH COUPON CODE: EDENMARCO
-        # For 20% Discount on all pricing
-        api_endpoint = "https://api.scrapin.io/enrichment/profile"
+        api_endpoint = "https://api.scrapein.io/enrichment/profile"
         params = {
-            "apikey": os.environ["SCRAPIN_API_KEY"],
-            "linkedInUrl": linkedin_profile_url,
+            "apiKey": os.getenv("SCRAPIN_API_KEY"),
+            "linkedinUrl": linkedin_profile_url,
         }
-        response = requests.get(
-            api_endpoint,
-            params=params,
-            timeout=10,
-        )
+        response = requests.get(api_endpoint, params=params, timeout=10)
 
     data = response.json().get("person")
-    data = {
-        k: v
-        for k, v in data.items()
-        if v not in ([], "", "", None) and k not in ["certifications"]
-    }
-
-    return data
-
 
 if __name__ == "__main__":
     print(
-        scrape_linkedin_profile(
-            linkedin_profile_url="https://www.linkedin.com/in/eden-marco/"
-        ),
+        scrape_linkedin_profile(linkedin_profile_url="https://www.linkedin.com/in/samirsatam", mock=True)
     )
