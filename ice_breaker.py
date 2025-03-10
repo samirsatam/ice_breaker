ce26faf8260e84caf6510c402b2b8904df079076
Samir Satam <ssatam@gmail.com>
2025-03-09 23:21:02 -0400

    Small fixes.

--- ice_breaker.py
+++ ice_breaker.py
@@ -13,19 +13,17 @@ if __name__ == "__main__":
     """
 
     information = """
-   George Washington Carver (c. 1864[1] – January 5, 1943) was an American agricultural scientist and inventor who promoted alternative crops to cotton and methods to prevent soil depletion.[2] He was one of the most prominent black scientists of the early 20th century.
-
-While a professor at Tuskegee Institute, Carver developed techniques to improve types of soils depleted by repeated plantings of cotton. He wanted poor farmers to grow other crops, such as peanuts and sweet potatoes, as a source of their own food and to improve their quality of life.[3] Under his leadership, the Experiment Station at Tuskegee published over forty practical bulletins for farmers, many written by him, which included recipes; many of the bulletins contained advice for poor farmers, including combating soil depletion with limited financial means, producing bigger crops, and preserving food.
-
-Apart from his work to improve the lives of farmers, Carver was also a leader in promoting environmentalism.[4] He received numerous honors for his work, including the Spingarn Medal of the NAACP. In an era of high racial polarization, his fame reached beyond the black community. He was widely recognized and praised in the white community for his many achievements and talents. In 1941, Time magazine dubbed Carver a "Black Leonardo".[5] 
+    George Washington Carver (c. 1864[1] – January 5, 1943) was an American agricultural scientist and inventor who promoted alternative crops to cotton and methods to prevent soil depletion.[2] He was one of the most prominent black scientists of the early 20th century.
+    While a professor at Tuskegee Institute, Carver developed techniques to improve types of soils depleted by repeated plantings of cotton. He wanted poor farmers to grow other crops, such as peanuts and sweet potatoes, as a source of their own food and to improve their quality of life.[3] Under his leadership, the Experiment Station at Tuskegee published over forty practical bulletins for farmers, many written by him, which included recipes; many of the bulletins contained advice for poor farmers, including combating soil depletion with limited financial means, producing bigger crops, and preserving food.
+    Apart from his work to improve the lives of farmers, Carver was also a leader in promoting environmentalism.[4] He received numerous honors for his work, including the Spingarn Medal of the NAACP. In an era of high racial polarization, his fame reached beyond the black community. He was widely recognized and praised in the white community for his many achievements and talents. In 1941, Time magazine dubbed Carver a "Black Leonardo".[5] 
     """
 
     summary_prompt_template = PromptTemplate(
         input_variables=["information"], template=summary_template
     )
-    # llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
+    llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
     # llm = ChatOllama(model="llama3.1")
-    llm = ChatOllama(model="mistral")
+    # llm = ChatOllama(model="mistral")
     # create a chain
     chain = summary_prompt_template | llm
     res = chain.invoke(input={"information": information})
--- third_parties/linkedin.py
+++ third_parties/linkedin.py
@@ -12,16 +12,24 @@ def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = False):
         linkedin_profile_url= "https://gist.githubusercontent.com/samirsatam/e0b9d14cae4e8b32edd3f2504f08d5b8/raw/b9e78b571af516e8c7685da1539dca6e63543383/samir-satam-scrapin.json"
         response = requests.get(linkedin_profile_url, timeout=10)
     else:
-        api_endpoint = "https://api.scrapein.io/enrichment/profile"
+        api_endpoint = "https://api.scrapin.io/enrichment/profile"
         params = {
-            "apiKey": os.getenv("SCRAPIN_API_KEY"),
-            "linkedinUrl": linkedin_profile_url,
+            "apikey": os.getenv("SCRAPIN_API_KEY"),
+            "linkedInUrl": linkedin_profile_url,
         }
         response = requests.get(api_endpoint, params=params, timeout=10)
 
     data = response.json().get("person")
+    # remove empty and redundant fields.
+    data = {
+        k: v for k, v in data.items()
+        if v not in ([], "", "", None)
+           and k not in ["certifications"]
+    }
+    print("Data returned:")
+    print(data)
 
 if __name__ == "__main__":
     print(
-        scrape_linkedin_profile(linkedin_profile_url="https://www.linkedin.com/in/samirsatam", mock=True)
+        scrape_linkedin_profile(linkedin_profile_url="https://www.linkedin.com/in/samirsatam/", mock=True)
     )
